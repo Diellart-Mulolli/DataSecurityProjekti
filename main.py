52642cf0d9bcf9238660f6cb925c95be4dee295a
drenxhyliqi <113259185+drenxhyliqi@users.noreply.github.com>
2025-04-19 20:24:33 +0200

    Initial project setup with placeholder comments for future development

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
-print("Testing github for commit!")
+# Importimet dhe Funksionet për Trajtimin e Karaktereve - Delvina Elshani
+
+
+
+# Ndihmës Shtesë dhe Logjika e Shifrimit - Diellart Mulolli
+
+
+
+
+# Programi Kryesor - Përpunimi i Hyrjes - Dren Xhyliqi
+
+
+
+
+
+# Programi Kryesor - Shifrimi dhe Dalja -  Diell Fazliu
