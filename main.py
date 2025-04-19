f874d0e6ed1615979131f7564c484e76f855500b
DiellartMulolli <diellart.mulolli@student.uni-pr.edu>
2025-04-19 21:27:35 +0200

    function for resizing keys

--- main.py
+++ main.py
@@ -6,3 +6,12 @@ def denumerate(indices):
 
 def decharrify(chars):
     return "".join(chars)
+
+# key resize function
+# This function resizes the key to match the size of the plaintext or ciphertext
+# by repeating the key as many times as necessary and then slicing it to the correct size.
+# TEXTINPUT
+# KEYKEYLEY
+
+def key_resize(key, size):
+    return (key * (size // len(key) + 1))[:size]
